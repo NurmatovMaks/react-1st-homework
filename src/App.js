c228fd35c2f7cd71b9ca1d961217dd3dc40864d7
Maksat <maksatlevsha@gmail.com>
2021-10-25 19:25:55 +0600

    react 1st hw

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Section from "./components/section";
+
+
+function App() {
+  return (<>
+    <Header />
+    <Section />
+    <Footer />
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/section.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './section.css';
+
+const Section = () => {
+    return (
+
+        <img className="img" src="https://c.wallhere.com/photos/a2/59/Marussia_2009_B2_netcarshow_netcar_car_images_car_photo-433583.jpg!d" alt="" />
+
+    );
+};
+
+export default Section;
